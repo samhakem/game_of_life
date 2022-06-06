232afca614b1eef47f361c282e38a66c7f27d75c
Sam Hakem <samhakem@egmail.com>
2022-06-06 13:50:25 +0200

    first commit and scaffolding setup with main game logic

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # Any live cell with more than three live neighbours dies, as if by overpopulation.
 # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 # These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
-#
+
 # Any live cell with two or three live neighbours survives.
 # Any dead cell with three live neighbours becomes a live cell.
 # All other live cells die in the next generation. Similarly, all other dead cells stay dead.
@@ -31,13 +31,18 @@ def update(screen, cells, size, with_progress=False):
     for row, col in numpy.ndindex(cells.shape):
         # Apply game rules here: calculate number of alive neighbouring cells, check all cells surrounding current
         # alive cell if any others are alive
+        # Calculate from top left cell to bottom right cell any 1 cells, sdubstract the 1 cell that is alive
         alive = numpy.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+        # Sets cells to background color if not alive = 0 else sets to alive next color
         color = color_bg if cells[row, col] == 0 else color_alive_next
 
         if cells[row, col] == 1:
+            # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+            # Any live cell with more than three live neighbours dies, as if by overpopulation.
             if alive < 2 or alive > 3:
                 if with_progress:
                     color - color_die_next
+            # Any live cell with two or three live neighbours lives on to the next generation.
             elif 2 <= alive <= 3:
                 updated_cells[row, col] = 1
                 if with_progress:
@@ -48,3 +53,5 @@ def update(screen, cells, size, with_progress=False):
                 color = color_alive_next
         pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
         return updated_cells
+
+# Main functionality
